188af6965728c1a97a5a1594292017afdaea575b
Zach McFadden <zach.mcfadden@workday.com>
2020-11-23 16:42:35 -0800

    oops

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ apt-get install python3
 # Install pip and this app's dependencies
 python3 -m pip install -U pip
 python3 -m pip install setuptools
-python3 -m pip install ./
+python3 ./setup.py install
 
